6a40e74b90b21d428864240135423f5b9928bd6b
Sergey <fhlbc2012@gmail.com>
2020-07-15 06:26:33 +0300

    init new project

---
+++ app/Http/Controllers/Api/PatientController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Patient;
+use Illuminate\Http\Request;
+
+class PatientController extends Controller
+{
+    public function index()
+    {
+        $patients = Patient::all();
+        return response()->json($patients);
+    }
+}
